f7c80cc4f371bb9d2f50559cc3b151d50229f69a
dyachaliin <dyachaliin@gmail.com>
2022-10-30 21:37:30 +0200

    Initial commit

---
+++ BitcoinWallet/Code/Views/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BitcoinWallet
+//
+//  Created by Alina Diachenko on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
